b7c3bdaa52b34948c4caaa56dc844dbc1c28ea9e
ragnabt <rajhona.gabor@gmail.com>
2021-11-11 14:40:08 +0100

    camera motion sensor settings

---
+++ capture_with_camera.py
@@ -0,0 +1,63 @@
+import cv2, time, cv2.cv2
+from datetime import datetime
+
+video = cv2.VideoCapture(1)
+# video = cv2.VideoCapture("gyak.mp4")
+status_list = [None, None]
+first_frame = None
+times = []
+# i = 0
+while True:
+    # i += 1
+    check, frame = video.read()
+    status = 0
+
+    gray = cv2.cv2.cvtColor(frame, cv2.cv2.COLOR_BGR2GRAY)
+    gray = cv2.cv2.GaussianBlur(gray, (21, 21), 0)
+
+    if first_frame is None:
+        first_frame = gray
+        continue
+
+    delta_frame = cv2.cv2.absdiff(first_frame, gray)
+
+    # I can set the sensitivity for the camera in the next line between the "delta_frama" and the "255"
+    thresh_frame = cv2.cv2.threshold(delta_frame, 150, 255, cv2.cv2.THRESH_BINARY)[1]
+    # -------------------------------------------------------------------------------------------------
+
+    thresh_delta = cv2.cv2.dilate(thresh_frame, None, iterations=2)
+    (cnts, _) = cv2.findContours(thresh_frame.copy(), cv2.cv2.RETR_EXTERNAL, cv2.cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv2.contourArea(contour) < 10000:
+            continue
+        status = 1
+
+        (x, y, w, h) = cv2.cv2.boundingRect(contour)
+        cv2.cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    print(status)
+    status_list.append(status)
+
+    if status_list[-1] == 1 and status_list[-2] == 0:
+        times.append(datetime.now())
+    if status_list[-1] == 0 and status_list[-2] == 1:
+        times.append(datetime.now())
+
+    # time.sleep(1)
+    cv2.cv2.imshow("Capturing", gray)
+    cv2.cv2.imshow("Delta frame", delta_frame)
+    cv2.cv2.imshow("Threshold Frame", thresh_frame)
+    cv2.cv2.imshow("Color frame", frame)
+
+    key = cv2.cv2.waitKey(1)
+    if key == ord('q'):
+        if status == 1:
+            times.append(datetime.now())
+        break
+
+print(status_list)
+print(times)
+
+video.release()
+
+cv2.cv2.destroyAllWindows()
